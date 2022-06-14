7472f15a1ff58ae0f769ddf680e36a51dd720b7c
dslatsh <106122071+dslatsh@users.noreply.github.com>
2022-06-13 23:27:05 -0400

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,3 +1,11 @@
+"""
+  Title: mongodb_test.py
+  Author: Devin Latshaw
+  Date: 6/12/2022
+  Description: Creates a connction to the mongoDB using the below connection string
+               it will then connect to the client.pytech database and print out the collection list
+"""
+
 from pymongo import MongoClient
 url = "mongodb+srv://admin:admin@cluster0.jnvyvcc.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(url)
